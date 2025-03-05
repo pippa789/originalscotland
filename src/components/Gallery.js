a05aca0b7385942501a3aa0ce346d797ff4f2edf
unknown <allenpip341@gmail.com>
2025-03-05 11:54:05 +0000

    set up gallery and modals

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,20 +1,44 @@
 import React from 'react';
-// import './images/pheonix.png';
-// import './images/seal1.png'
+import {useState} from 'react';
+import '../index.css';
+
+
+const images = [
+  { src: '/images/seal-gallery.png', alt: 'watercolor of a seal', className:'seal-gallery'},
+  { src: '/images/dunoon-gallery.png', alt: 'beach scene at dunoon'},
+  { src:'/images/fox-gallery.png', alt: 'a fox in the burn'},
+  {src: '/images/south-gourock.png', alt:'south guorock'},
+  {src:'/images/pheonix-gallery.png', alt: 'pheonix'},
+  {src:'/images/sky-gallery.png', alt:'the isle of sky'},
+  {src:'/images/crow-gallery.png', alt:'coastal scene with crow'},
+  { src:'/images/cumbrea-gallery.png', alt:'the isle of cumbrea'},
+  {src:'/images/summers-evening-g.png', alt: 'the woods on a summers evening'},
+]
+
+
 
 function Gallery() {
+const [SelectedImage, setSelectedImage] = useState(null);
   return (
     <div className='container'>
-      {/* <div className='pheonix'></div> */}
-      <div className='seal-gallery'></div>
-      <div className='dunoon-gallery'></div>
-      <div className='fox-gallery'></div>
-      <div className='south-gourock'></div>
-      <div className='pheonix-gallery'></div>
-      <div className='sky-gallery'></div>
-      <div className='crow-gallery'></div>
-      <div className='cumbrea-gallery'></div>
-      <div className='battery-park-g'></div>
+      {images.map((image, index)=>(
+        <img
+        key={index}
+        src={image.src}
+        alt={image.alt}
+        className={`image ${image.className}`} 
+        onClick={()=>setSelectedImage(image.src)}
+        />
+      ))}
+
+<div className={`modal-overlay ${SelectedImage ? "show" : ""}`} onClick={() => setSelectedImage(null)}>
+        {SelectedImage && (
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <img src={SelectedImage} alt="Selected Artwork" className="seal-modal" />
+            <button className="close-btn" onClick={() => setSelectedImage(null)}>✕</button>
+          </div>
+      )}
+     </div>
     </div>
   )
 }
