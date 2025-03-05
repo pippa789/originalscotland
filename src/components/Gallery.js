7f443dbc58ff41bc417c0de57ad9da6524385754
unknown <allenpip341@gmail.com>
2025-03-05 17:00:03 +0000

    cannot access the image array from the modal

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,44 +3,48 @@ import {useState} from 'react';
 import '../index.css';
 
 
+
 const images = [
-  { src: '/images/seal-gallery.png', alt: 'watercolor of a seal', className:'seal-gallery'},
-  { src: '/images/dunoon-gallery.png', alt: 'beach scene at dunoon'},
-  { src:'/images/fox-gallery.png', alt: 'a fox in the burn'},
-  {src: '/images/south-gourock.png', alt:'south guorock'},
-  {src:'/images/pheonix-gallery.png', alt: 'pheonix'},
-  {src:'/images/sky-gallery.png', alt:'the isle of sky'},
-  {src:'/images/crow-gallery.png', alt:'coastal scene with crow'},
-  { src:'/images/cumbrea-gallery.png', alt:'the isle of cumbrea'},
-  {src:'/images/summers-evening-g.png', alt: 'the woods on a summers evening'},
+  {src: '/images/seal-gallery.png', alt: 'watercolor of a seal', className:'seal-gallery'},
+  {src: '/images/dunoon-gallery.png', alt: 'beach scene at dunoon',className:'dunoon-gallery'},
+  {src:'/images/fox-gallery.png', alt: 'a fox in the burn', className:'fox-gallery'},
+  {src: '/images/south-gourock.png', alt:'south guorock', className:'south-gourock'},
+  {src:'/images/pheonix-gallery.png', alt: 'pheonix', className:'pheonix-gallery'},
+  {src:'/images/sky-gallery.png', alt:'the isle of sky', className:'sky-gallery'},
+  {src:'/images/crow-gallery.png', alt:'coastal scene with crow', className:'crow-gallery'},
+  {src:'/images/cumbrea-gallery.png', alt:'the isle of cumbrea', className:'cumbrea-gallery'},
+  {src:'/images/summers-evening-g.png', alt: 'the woods on a summers evening', className:'summers-evening-g'},
 ]
 
 
 
 function Gallery() {
-const [SelectedImage, setSelectedImage] = useState(null);
+const [SelectedImage, setSelectedImage] = useState(false);
+
+
   return (
     <div className='container'>
       {images.map((image, index)=>(
         <img
         key={index}
-        src={image.src}
+        src={images.src}
         alt={image.alt}
         className={`image ${image.className}`} 
-        onClick={()=>setSelectedImage(image.src)}
+        onClick={()=> setSelectedImage(...images)} 
         />
       ))}
-
-<div className={`modal-overlay ${SelectedImage ? "show" : ""}`} onClick={() => setSelectedImage(null)}>
-        {SelectedImage && (
+      
+    {SelectedImage && (
+        <div className="modal-overlay show" onClick={() => setSelectedImage(null)}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <img src={SelectedImage} alt="Selected Artwork" className="seal-modal" />
+            <img src={SelectedImage} alt={SelectedImage}className="modal-image" />
             <button className="close-btn" onClick={() => setSelectedImage(null)}>✕</button>
           </div>
+        </div>
       )}
      </div>
-    </div>
-  )
+      )
 }
+
 
 export default Gallery
