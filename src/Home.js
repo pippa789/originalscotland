cb4f7c620e338bd9d71536a77da3de44d1eaa62e
unknown <allenpip341@gmail.com>
2025-03-16 19:28:00 +0000

    tinkering with responsive css

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ import GalleryButton from './components/GalleryButton'
 function Home() {
   return (
   <>
+    
     <div className='title'> 
       Welcome to Original Scotland 
       <div className='home-text'>I'm Pippa Bullock, an artist living and working amidst the magic of Scotland's West Coast and Hebrides. Here, the oldtimers say the veil between our world and the divine is "thin," and it's in this space that I create. For me, painting a landscape isn't about replicating a scene; it's about interpreting nature's voice and sharing that conversation with you through each piece I create. Browse my gallery to discover more.</div>
